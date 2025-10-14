662c27b0301535bbb7599655b29cb0c5d02e3ce0
oojas <oojasgupta@gmail.com>
2025-10-14 08:52:56 +0530

    Shifting everything in section 2 folder

--- section2/cards/src/main/java/com/example/cards/dto/ResponseDTO.java
+++ section2/cards/src/main/java/com/example/cards/dto/ResponseDTO.java
@@ -2,9 +2,10 @@ package com.example.cards.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
+@Data
 public class ResponseDTO {
     private String statusCode;
     private String statusMsg;
