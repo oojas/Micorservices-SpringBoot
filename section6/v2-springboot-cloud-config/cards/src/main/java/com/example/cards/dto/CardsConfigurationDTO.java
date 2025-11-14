683847c599070537997587f9a302c27c7c60e192
oojas <oojasgupta@gmail.com>
2025-11-14 11:39:43 +0530

    Adding refresh configurations with Actuator and generated docker compose images

--- section6/v2-springboot-cloud-config/cards/src/main/java/com/example/cards/dto/CardsConfigurationDTO.java
+++ section6/v2-springboot-cloud-config/cards/src/main/java/com/example/cards/dto/CardsConfigurationDTO.java
@@ -1,10 +1,21 @@
 package com.example.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsConfigurationDTO(String message, HashMap<String,String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+// This class is getting used because we can have changes at runtime for configurations and we need to reflect them without restarting the application
+public class CardsConfigurationDTO{
+    private String message;
+    private HashMap<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
+// This record class is used if we have configurations that cannot be changed, because in record classes you cannot change the value of variables.
+//public record CardsConfigurationDTO(String message, HashMap<String,String> contactDetails, List<String> onCallSupport) {
+//}
--- section6/v2-springboot-cloud-config/loans/src/main/java/com/microservices/loans/DTO/LoansConfigurationDTO.java
+++ section6/v2-springboot-cloud-config/loans/src/main/java/com/microservices/loans/DTO/LoansConfigurationDTO.java
@@ -1,10 +1,24 @@
 package com.microservices.loans.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansConfigurationDTO(String message, HashMap<String,String> contactDetails, List<String> onCallSupport) {
+
+@Getter
+@Setter
+// This class is getting used because we can have changes at runtime for configurations and we need to reflect them without restarting the application
+public class LoansConfigurationDTO{
+    private String message;
+    private HashMap<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
+
+
+// This record class is used if we have configurations that cannot be changed, because in record classes you cannot change the value of variables.
+//public record LoansConfigurationDTO(String message, HashMap<String,String> contactDetails, List<String> onCallSupport) {
+//}
